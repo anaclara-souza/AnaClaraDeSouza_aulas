79a4c4e8b16a7ffe3193b6393ce930a14ba54ee2
anaclara-souza <1234anasouza5678@gmail.com>
2025-02-06 21:59:58 -0300

    exemplo1.py
    primeiro exercicio

---
+++ exemplo2.py
@@ -0,0 +1,3 @@
+a = 2 # variavel a receber o valor 2;
+b = 3 # variavel a receber o valor 3;
+print (a + b) # print e uma funçao que exibe a soma de a + b;
---
+++ exemplo3.py
@@ -0,0 +1,4 @@
+# faça um programa que receba um salario de 1500 e tenha um aumento de cinco porcento do salario e ao final exiba o salario com o aumento
+salario = 1500
+salariofinal= salario+salario*0.05
+print(salariofinal)
