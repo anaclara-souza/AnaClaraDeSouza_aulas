828dc0cf3086e9f51537dae5e807cd1945d29003
anaclara-souza <1234anasouza5678@gmail.com>
2025-02-06 21:32:00 -0300

    aula01.py
    so da errado

---
+++ exercicio01.py
@@ -0,0 +1,8 @@
+x = 34 - 23 #um comentario
+y = "hello" #outrocomentario
+z = 3.45
+if z == 3.45 or y == "hello":
+    x = x + 1
+    y = y + " world" #concatenaçao de strings
+    print(x)
+    print(y)
