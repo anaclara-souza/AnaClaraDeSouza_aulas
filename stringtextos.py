46594813aad88baa2de09ee7ce8e320da156df51
anaclara-souza <1234anasouza5678@gmail.com>
2025-02-10 20:02:07 -0300

    atividade

---
+++ stringtextos.py
@@ -0,0 +1,7 @@
+email = '1234anasouza5678@gmail.com'
+
+print("\npegando posiçao ZERO no email atribuido:")
+print(email[0])
+
+print("\nPegando da posiçao ZERO ate a ONZE do email atribuido:")
+print(email[0:11])
---
+++ tabstring.py
@@ -0,0 +1,4 @@
+print("payton")
+print("\tPayton")
+#para acrecentar uma quebra de linha utiliza a combinaçao de caractereres \n:
+print("Tipo de linguagens:\nPayton\nC\nJavaScript")
