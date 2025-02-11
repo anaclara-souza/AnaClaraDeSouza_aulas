0c6d1a8389587fad6b812a1044b0ddefab4e87c9
anaclara-souza <1234anasouza5678@gmail.com>
2025-02-11 18:59:33 -0300

    atividade

--- exemploif.py
+++ exemploif.py
@@ -1,2 +1,8 @@
-num = 10
-if num > 10
+num = int(input("Digite seu numero: "))
+if num > 10:
+   print("O numero è maior que 10")
+else:
+    if num == 10:
+        print("O numero é igual a 10")
+    else:
+        print("O numero é menor que 10")
