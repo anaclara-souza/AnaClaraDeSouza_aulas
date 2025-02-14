620c91bc59649b2337e173bee731e73574c45eb2
anaclara-souza <1234anasouza5678@gmail.com>
2025-02-14 20:32:49 -0300

    atividades

---
+++ exemplo001lista.py
@@ -0,0 +1,8 @@
+lista1 = [1,2,3,4,5]
+lista2 = [6,7,8,9,10]
+lista3 = [11,12,13,14,15]
+todas_listas = [lista1,lista2,lista3]
+print(todas_listas)
+
+produtos = ['tv','celular','mouse','teclado','tablete']
+print(produtos[1])
