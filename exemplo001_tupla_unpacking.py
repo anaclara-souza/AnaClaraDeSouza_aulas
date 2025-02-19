33349e3e17fa0a2f9954cadaa08f75be5879f32c
anaclara-souza <1234anasouza5678@gmail.com>
2025-02-19 19:18:31 -0300

    atividades

---
+++ exemplo001_tupla_unpacking.py
@@ -0,0 +1,9 @@
+#forma mais facil de fazera tupla.
+
+venda = ('ana','10/03/2025','28/03/2008','785','Jovem aprenndiz')
+nome,data_contratacao,data_nacimento,salario,cargo = venda
+print(nome)
+print(data_contratacao)
+
+print(salario)
+print(cargo)
